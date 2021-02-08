df5ebcae153325a4f49ecd18d9ce9c88d05e9354
ssadata <60407190+ssadata@users.noreply.github.com>
2021-02-08 17:23:01 -0300

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,30 @@
+##############################################
+# # # LIBRERIAS # # #
+
+from pdf2docx import Converter
+import os
+
+##############################################
+# # # CARGA DE TEXTOS # # # 
+
+# # # Definición del directorio donde se alojarán los textos convertidos # # #
+
+path_input = '/pdftodocx/input/'
+path_output = '/pdftodocx/output/'
+
+# # # Loop para leer todos los documentos, reconvertirlos y guardarlos # # #
+
+for file in os.listdir(path_input):
+	cv = Converter(path_input+file)
+	cv.convert(path_output+file+'.docx', start=0, end=None)
+	cv.close()
+	print(file)
+
+
+###############################################
+"""
+El resultado es la lectura de todas los archivos disponibles en PDF y guardandolos
+en formato .docx para poder ser editados
+"""
+###############################################
+print("Fin del Proceso")
